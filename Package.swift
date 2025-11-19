60cb6e9be2a378bfc26431be6e5f2915550b24b0
Coen ten Thije Boonkkamp <coen@coenttb.com>
2025-11-19 09:26:27 +0100

    Fix INCITS 4-1986 dependency version to 0.0.1

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/swift-standards/swift-incits-4-1986", from: "0.1.0")
+        .package(url: "https://github.com/swift-standards/swift-incits-4-1986", from: "0.0.1")
     ],
     targets: [
         .target(
